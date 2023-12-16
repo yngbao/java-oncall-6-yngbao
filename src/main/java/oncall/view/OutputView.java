4e9a7c4bb82993c79e7219bbbcf0287e4e7071d1
육예진 <yngbao@yug-yejin-ui-MacBookAir.local>
2023-12-16 14:36:47 +0900

    feat: 출력 틀 생성

--- src/main/java/oncall/view/OutputView.java
+++ src/main/java/oncall/view/OutputView.java
@@ -1,5 +1,13 @@
 package oncall.view;
 
 public class OutputView {
-
+	public void printDayAndWorker(Boolean isHolidayOnWeekday) {
+		if (isHolidayOnWeekday) {
+			System.out.println();
+		}
+		if (!isHolidayOnWeekday) {
+			System.out.println();
+		}
+	}
+	
 }
--- src/main/java/oncall/view/message/SystemMessage.java
+++ src/main/java/oncall/view/message/SystemMessage.java
@@ -3,7 +3,9 @@ package oncall.view.message;
 public enum SystemMessage {
 	INPUT_MONTH_SETTING("비상 근무를 배정할 월과 시작 요일을 입력하세요>"),
 	INPUT_WEEKDAY_PEOPLE("평일 비상 근무 순번대로 사원 닉네임을 입력하세요>"),
-	INPUT_HOLIDAY_PEOPLE("휴일 비상 근무 순번대로 사원 닉네임을 입력하세요>");
+	INPUT_HOLIDAY_PEOPLE("휴일 비상 근무 순번대로 사원 닉네임을 입력하세요>"),
+	DAY_FORM("%d월 %d일 %s"),
+	HOLIDAY("(휴일)");
 	
 	private final String message;
 	
