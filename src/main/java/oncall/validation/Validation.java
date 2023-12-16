9cdbea2b0bb10efbcc3736bce231787ff2076ed5
육예진 <yngbao@yug-yejin-ui-MacBookAir.local>
2023-12-16 15:01:52 +0900

    feat: 첫 입력값을 나눈 문자열 배열의 크기가 2가 아닌 경우 예외발생

--- src/main/java/oncall/validation/Validation.java
+++ src/main/java/oncall/validation/Validation.java
@@ -6,6 +6,12 @@ public class Validation {
 	private static final int FIRST_INPUT_SIZE = 2;
 	private static final char SPLIT_CHARACTER = ',';
 	
+	public static void validateFirstInputSize(String[] inputs) {
+		if (!isValidSize(inputs)) {
+			throw new IllegalArgumentException();
+		}
+	}
+	
 	public static void validateDayOfWeek(String input) {
 		if (!isValidDayName(input)) {
 			throw new IllegalArgumentException();
@@ -16,6 +22,10 @@ public class Validation {
 		if (!isValidMonth(date)) {
 			throw new IllegalArgumentException();
 		}
+	}
+	
+	private static boolean isValidSize(String[] inputs) {
+		return inputs.length == FIRST_INPUT_SIZE;
 	}
 	
 	private static boolean isValidMonth(int date) {
